36c43b8a6498368072dd362c5809d8d941776a70
Leticia Cibele <leticiacibele.cl@gmail.com>
2024-11-13 21:10:20 -0300

    feat: aprendendo a adicionar o html no react

--- 1_INTRODUCAO/hello-world/src/App.js
+++ 1_INTRODUCAO/hello-world/src/App.js
@@ -5,6 +5,15 @@ function App() {
   return (
     <div className="App">
       <h1>Hello world React</h1>
+      <h2 className="subtitulo">Aprendendo React</h2>
+      <h3>Lista de Tecnologias:</h3>
+      <ul>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JavaScript</li>
+        <li>Bootstrap</li>
+        <li>React</li>
+      </ul>
     </div>
   );
 }
